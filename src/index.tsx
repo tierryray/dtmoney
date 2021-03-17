53eb5dff0729c13ed372df88de7c3bc9758e1cd9
Tierry Ray <tierry.ray@ifood.com.br>
2021-03-17 12:16:56 -0300

    feat(miragejs): add miragejs for fake backend on application

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,26 @@
 import { App } from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
