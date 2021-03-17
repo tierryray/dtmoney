7304357fddc57a0fe09775d05050199361b9cd7f
Tierry Ray <tierry.ray@ifood.com.br>
2021-03-17 12:22:22 -0300

    feat(axios): add axios to fetch data

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,11 @@
 import { Container } from "./styles";
+import { api } from "../../services/api";
 import { useEffect } from "react";
 
 export function TransactionTable() {
   useEffect(() => {
-    fetch('http://localhost:3000/api/transactions')
-      .then(response => response.json())
-      .then(data => console.log(data))
+    api.get('transactions')
+      .then(response => console.log(response.data))
   }, []);
 
   return (
