149746983dd3020bb3207285b5828b79d9299adf
Tierry Ray <tierry.ray@ifood.com.br>
2021-03-18 17:53:01 -0300

    feat(modal): modal style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import { Dashboard } from "./components/Dashboard"
 import { GlobalStyle } from "./styles/global"
 import { Header } from "./components/Header"
+import Modal from "react-modal"
+import { NewTransactionModal } from "./components/NewTransactionModal";
+import { useState } from "react";
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen} 
+        onRequestClose={handleCloseNewTransactionModal} 
+      />
+
       <GlobalStyle />
     </>
-  )
+  );
 }
